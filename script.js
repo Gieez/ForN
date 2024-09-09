1e8e6aec9e5e8d1a6cd7023bbbf772d612383399
Gieez <124506459+Gieez@users.noreply.github.com>
2024-09-09 13:08:57 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('revealMessage').addEventListener('click', function () {
+    const specialMessage = document.getElementById('specialMessage');
+    specialMessage.classList.toggle('hidden');
+});
